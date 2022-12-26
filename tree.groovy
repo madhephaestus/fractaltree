4dbb8e8b48274ab9c5b160372741dfcdea78218f
Kevin Harrington <mad.hephaestus@gmail.com>
2022-12-26 17:46:22 -0500

    flowers!

--- tree.groovy
+++ tree.groovy
@@ -5,19 +5,34 @@ import com.neuronrobotics.sdk.addons.kinematics.math.TransformNR
 import eu.mihosoft.vrl.v3d.CSG
 import eu.mihosoft.vrl.v3d.Cube
 import eu.mihosoft.vrl.v3d.Transform
-
-//Your code here
 branchLength =  60
-
-				
 ArrayList<CSG> branches  = []
-
 fractalDepth = 4
 TransformNR brancLoc = new TransformNR()
 
+CSG leif(def branches,TransformNR brancLocation) {
+	CSG branch = new Cube(20,10,5).toCSG()
+	.toZMin().toXMin()
+	CSG flower = branch
+	for(int i=0;i<360;i+=(360/5)) {
+		flower= flower.union(branch.rotz(i))
+	}
+	def tip=brancLocation.times(
+		new TransformNR(0,0,branchLength,new RotationNR()))
+	return flower.transformed(TransformFactory.nrToCSG(tip))
+}
+
 void makeBranches(def branches, TransformNR brancLocation,int currentDepth, def y, def x,def z) {
-	if(currentDepth==0)
+	if(currentDepth==1) {
+		CSG flower = leif( branches, brancLocation)
+		for(int i=0;i<branches.size();i++) {
+			if(flower.touching(branches.get(i))) {
+				return;
+			}
+		}
+		branches.add(flower)
 		return
+	}
 	def myLen = branchLength*(currentDepth)
 	branch = new Cube(5,5,branchLength*(currentDepth-1)).toCSG()
 	.toZMin()
@@ -34,7 +49,5 @@ void makeBranches(def branches, TransformNR brancLocation,int currentDepth, def 
 	makeBranches(branches,left,currentDepth-1, y,x,z)
 	makeBranches(branches,right,currentDepth-1, y,x,z)
 }
-
-makeBranches(branches,brancLoc,4, 20,20,60)
-
+makeBranches(branches,brancLoc,5, 30,20,60)
 return branches
